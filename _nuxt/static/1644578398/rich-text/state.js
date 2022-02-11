397a35a948d232e71f5fffc10e4d046001c15649
ThornWalli <ThornWalli@users.noreply.github.com>
2022-02-11 11:21:39 +0000

    deploy: 8dc53959a10bd3ed763cff5026d92fe7afcb87c5

--- _nuxt/static/1644578398/rich-text/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffoundation\u002F_nuxt\u002Fstatic\u002F1644578398",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Frich-text",config:{_app:{basePath:"\u002Ffoundation\u002F",assetsPath:"\u002Ffoundation\u002F_nuxt\u002F",cdnURL:a}}}}(null));
