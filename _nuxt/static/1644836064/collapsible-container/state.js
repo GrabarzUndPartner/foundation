26eeae83ecf8f1f4e2f1936366b3bc7f1f5183ae
ThornWalli <ThornWalli@users.noreply.github.com>
2022-02-14 10:55:17 +0000

    deploy: 7e5f17ef1c5a5a58af06cdcd20816e1f2370a4d8

---
+++ _nuxt/static/1644836064/collapsible-container/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffoundation\u002F_nuxt\u002Fstatic\u002F1644836064",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fcollapsible-container",config:{_app:{basePath:"\u002Ffoundation\u002F",assetsPath:"\u002Ffoundation\u002F_nuxt\u002F",cdnURL:a}}}}(null));
