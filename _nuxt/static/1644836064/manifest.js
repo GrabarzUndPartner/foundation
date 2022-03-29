a4b5886487f3064918503398d64659e746488afe
ThornWalli <ThornWalli@users.noreply.github.com>
2022-03-29 08:44:57 +0000

    deploy: aa6586bcefecc92827e1da1f44f532d3870e1f9d

--- _nuxt/static/1644836064/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fbutton","\u002Fcollapsible-container","\u002Fdialog","\u002Fheadline","\u002Flink","\u002Ftoggle","\u002Frich-text","\u002Fslider","\u002F","\u002Farticle"]})
