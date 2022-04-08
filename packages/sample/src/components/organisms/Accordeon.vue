7995128ce9306d28824dc5538190c1bd6fab9f86
Jan Szidat <jan.szidat@grabarzundpartner.de>
2022-04-08 11:41:03 +0200

    feat(components): sample accordion

--- packages/sample/src/components/organisms/Accordeon.vue
+++ packages/sample/src/components/organisms/Accordeon.vue
@@ -1,26 +1,35 @@
 <template>
   <layout-lost-container class="organism-accordeon">
-    <molecule-accordeon />
+    <molecule-accordeon class="accordeon" :items="items" />
   </layout-lost-container>
 </template>
 
 <script>
 import MoleculeAccordeon from '@/components/molecules/Accordeon';
 import LayoutLostContainer from '@/components/layouts/LostContainer';
+
 export default {
   components: { MoleculeAccordeon, LayoutLostContainer },
   props: {
-    overline: {
-      type: String,
-      default: 'Overline'
-    },
-    headline: {
-      type: String,
-      default: 'Headline'
-    },
-    content: {
-      type: String,
-      default: '<p>Text</p>'
+    items: {
+      type: Array,
+      required: true,
+      default () {
+        return [
+          {
+            label: 'Item 1',
+            value: 'item1',
+            content: [
+              {
+                type: 'RichText',
+                props: {
+                  text: 'Lorem Ipsum item 1'
+                }
+              }
+            ]
+          }
+        ];
+      }
     }
   }
 };
@@ -32,6 +41,11 @@ export default {
 
   @media (--sm) {
     margin: em(48) 0;
+  }
+
+  & .accordeon {
+    lost-offset: 1 / 12;
+    lost-column: 10 / 12;
   }
 }
 
--- packages/sample/src/pages/index.vue
+++ packages/sample/src/pages/index.vue
@@ -15,7 +15,8 @@ export default {
     OrganismText: speedkitHydrate(() => import('@/components/organisms/Text')),
     OrganismGallery: speedkitHydrate(() => import('@/components/organisms/Gallery')),
     OrganismTextImage: speedkitHydrate(() => import('@/components/organisms/TextImage')),
-    OrganismTeaserWall: speedkitHydrate(() => import('@/components/organisms/TeaserWall'))
+    OrganismTeaserWall: speedkitHydrate(() => import('@/components/organisms/TeaserWall')),
+    OrganismAccordeon: speedkitHydrate(() => import('@/components/organisms/Accordeon'))
 
   },
   data () {
@@ -90,6 +91,71 @@ export default {
             overline: 'Sample Overline',
             headline: 'Sample Headline',
             content: '<p>Lorem ipsum dolor sit amet, <strong>consetetur sadipscing elitr</strong>, <em>sed diam nonumy eirmod tempor invidunt</em> <strong><em>ut labore et dolore magna aliquyam erat</em></strong>, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, <strong>consetetur sadipscing elitr</strong>, sed diam</p> <p>Lorem ipsum dolor sit amet, <strong>consetetur sadipscing elitr</strong>, <em>sed diam nonumy eirmod tempor invidunt</em></p> <ul> <li>List Item 1</li> <li>List Item 2</li> <li>List Item 3</li> </ul> <ol> <li>List Item 1</li> <li>List Item 2</li> <li>List Item 3</li> </ol>'
+          }
+        },
+        {
+          component: 'OrganismAccordeon',
+          props: {
+            items: [
+              {
+                label: 'Item 1',
+                value: 'item1',
+                content: [
+                  {
+                    type: 'Headline',
+                    props: {
+                      overline: 'This is an overline',
+                      text: 'This is the headline.'
+                    }
+                  },
+                  {
+                    type: 'RichText',
+                    props: {
+                      value: '<p>RichText Lorem Ipsum item 1</p>'
+                    }
+                  },
+                  {
+                    type: 'Link',
+                    props: {
+                      title: 'Sample Link',
+                      to: {
+                        hash: 'sample'
+                      }
+                    }
+                  }
+                ]
+              },
+              {
+                label: 'Item 2',
+                value: 'item2',
+                content: [
+                  {
+                    type: 'RichText',
+                    props: {
+                      value: '<p>RichText Lorem Ipsum item 2</p>'
+                    }
+                  },
+                  {
+                    type: 'Button',
+                    props: {
+                      label: 'Sample Button'
+                    }
+                  }
+                ]
+              },
+              {
+                label: 'Item 3',
+                value: 'item3',
+                content: [
+                  {
+                    type: 'RichText',
+                    props: {
+                      value: '<p>RichText Lorem Ipsum item 3</p>'
+                    }
+                  }
+                ]
+              }
+            ]
           }
         }
       ],
