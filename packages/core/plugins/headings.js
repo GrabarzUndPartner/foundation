a80b370d235cb8015af007d272c6b8a1eece82da
Jan Szidat <jan.szidat@grabarzundpartner.de>
2022-05-09 14:06:55 +0200

    feat(content): content component structure

---
+++ packages/core/plugins/headings.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import { DocumentSection, DocumentHeading } from 'vue-headings';
+
+Vue.component('DocumentSection', DocumentSection);
+Vue.component('DocumentHeading', DocumentHeading);
+
