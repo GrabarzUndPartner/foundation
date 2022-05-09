d85c8d301d3d37d8896a4f1fc56d51bee3259e24
Jan Szidat <jan.szidat@grabarzundpartner.de>
2022-05-09 14:09:43 +0200

    feat(content): content component structure

---
+++ packages/sample/src/components/layouts/Page.vue
@@ -0,0 +1,21 @@
+<template>
+  <layout-content
+    debug-id="page"
+    forced-tag="article"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </layout-content>
+</template>
+
+<script>
+import LayoutContent from '@/components/layouts/Content';
+
+export default {
+  components: {
+    LayoutContent
+  },
+  inheritAttrs: false
+};
+</script>
