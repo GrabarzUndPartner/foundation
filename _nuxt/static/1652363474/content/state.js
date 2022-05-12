c0fca25a9f3b0a8d6e52cc3481258c8584be5d69
ThornWalli <ThornWalli@users.noreply.github.com>
2022-05-12 13:52:06 +0000

    deploy: 33c3c2733bcfc75735536e6befe66a17b87820c8

---
+++ _nuxt/static/1652363474/content/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffoundation\u002F_nuxt\u002Fstatic\u002F1652363474",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fcontent",config:{_app:{basePath:"\u002Ffoundation\u002F",assetsPath:"\u002Ffoundation\u002F_nuxt\u002F",cdnURL:a}}}}(null));
